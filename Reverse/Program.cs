262559095abf7856a767a3f80945cb4948362043
Kaan Tosun <tr.kaantsn@gmail.com>
2023-03-19 00:14:32 +0300

    completed

---
+++ Reverse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen bir yazı yazın");
+            string output = System.Console.ReadLine();
+            string reversee = "";
+
+            for (int i = output.Length - 1; i >= 0; i--)
+            {
+                reversee += output.Substring(i, 1);
+            }
+
+            System.Console.WriteLine(reversee);
+
+            Console.ReadLine();
+        }
+          
+    }
+}
